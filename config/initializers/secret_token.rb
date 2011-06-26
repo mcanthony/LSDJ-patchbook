fc8349cf63f69d50a26d2f9f0254b7ba7a43e24d
Marc Lagrange <rhaamo@gruik.at>
2011-06-26 22:24:47 +0200

    First import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LsdjPatchbook::Application.config.secret_token = '81e15267d87d8dd7d6b942d9d34a472af3d847b0d6008b98ba180dd68c10c05714486794c9051139760a9d13683325b498bdcef12e2f41989c147fee1d7553c4'
