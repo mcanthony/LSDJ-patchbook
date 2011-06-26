cca5c64f93002633ec3853ad1b49170c668b8d87
Marc Lagrange <rhaamo@gruik.at>
2011-06-27 00:16:22 +0200

    Add dynamic_form plugin

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 LsdjPatchbook::Application.routes.draw do
+  resources :users, :user_sessions
+
+  match "/login", :to => "user_sessions#new"
+  match "/logout", :to => "user_sessions#destroy"
+
   root :to => "application#index"
 end
