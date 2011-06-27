6e7ee1870bacd7fb9248d7676ef6f28cc478f892
Marc Lagrange <rhaamo@gruik.at>
2011-06-27 15:25:16 +0200

    Show instruments tagged

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -2,4 +2,9 @@ class TagsController < ApplicationController
   def index
     @tags = Instrument.tag_counts_on(:tags)
   end
+
+  def show
+    @instruments = Instrument.page.tagged_with(params[:id])
+    render 'instruments/index'
+  end
 end
